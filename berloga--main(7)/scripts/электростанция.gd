1e202e58aff4fc506f483f13507abf0968b85cb1
Tverksaac <118615711+Tverksaac@users.noreply.github.com>
2024-11-28 18:09:50 +0300

    Delete berloga--main(7) directory

--- berloga--main(7)/scripts/электростанция.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	tree_entered.connect(func ():
-		GlobalVariables.free_electro.append(self)
-	)
