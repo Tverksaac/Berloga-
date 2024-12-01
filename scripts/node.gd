c7ab75f4f37feb2fa7583ae168b21bb2ccdcd40e
Niktarin59 <167767194+Niktarin59@users.noreply.github.com>
2024-12-01 16:43:33 +0700

    Update node.gd

--- scripts/node.gd
+++ scripts/node.gd
@@ -1,41 +1,44 @@
 extends Node
 
 @onready var pause: Panel = $"../player/Camera2D/CanvasLayer/Control/pp"
-var pause_b = false
+var GV = GlobalVariables
+var pa_panel = false
 
 func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("esc"):
-			pause_b = !pause_b
-		if pause_b == true:
+			GV.paus = !GV.paus
+			pa_panel = !pa_panel
+		if GV.paus == true:
+			if pa_panel: pause.show()
 			get_tree().paused = true
-			pause.show()
 			Engine.time_scale = 0
 			
 		else:
 			get_tree().paused = false
-			pause.hide()
+			if pa_panel == false:  pause.hide()
 			Engine.time_scale = 1
 			
 			
 			
 			
 func _on_start_pressed() -> void:
-	pause_b = !pause_b
+	GV.paus = !GV.paus
+	pa_panel = !pa_panel
 	Engine.time_scale = 1
 
 	
 		
 	
 func _on_pause_pressed() -> void:
-	pause_b = true
+	GV.paus = true
+	pa_panel = true
 	Engine.time_scale = 0	
 
 
 func _on_quit_pressed() -> void:
-	pause_b = false
-	
+	GV.paus = false
+	pa_panel = false
 	GlobalVariables.ResetGame(1000)
-	
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	
 	
